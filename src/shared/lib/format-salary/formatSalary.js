a2cda2b302e34c7fab0d7962de05854744ebdb92
screenfrisk <screenfriskofficial@gmail.com>
2024-01-07 03:26:09 +1100

    fix formatSalary.js

--- src/shared/lib/format-salary/formatSalary.js
+++ src/shared/lib/format-salary/formatSalary.js
@@ -1,21 +1,20 @@
 export const formatSalary = (salary_min, salary_max, salary, currency = "") => {
   currency = currency.replace("«", "");
   currency = currency.replace("»", "");
-  if (salary_max === undefined && salary_min === undefined && salary) {
+
+  if (salary_min === 0 && salary_max === 0 && salary) {
     return salary + " " + currency;
-  } else if (salary_min === salary_max) {
-    return `${
-      salary_min === undefined || salary_max === undefined
-        ? salary + " " + currency
-        : "З/П не указана."
-    }`;
-  } else if (salary_min && salary_max) {
-    return `от ${salary_min} до ${salary_max} ${currency}`;
-  } else if (salary_max) {
+  }
+  if (salary_min === salary_max) {
+    return salary_min + " " + currency || salary_max + " " + currency;
+  }
+  if (salary_min > 0 && salary_max > 0 && salary) {
+    return `от ${salary_min} до  ${salary_max} ${currency}`;
+  }
+
+  if (salary_min === 0 && salary_max > 0) {
     return `до ${salary_max} ${currency}`;
-  } else if (salary_min) {
-    return `от ${salary_min} ${currency}`;
   } else {
-    return "З/П не указана.";
+    return `от ${salary_min} ${currency}`;
   }
 };
